37a723ac2b05d765d0744c8d21ec43bf682221ab
robert.brzozowski <robert.brzozowski@ftsgps.com>
2022-10-14 09:12:54 +0200

    task solved

---
+++ InternetBankingRESTfulService.Api/InternetBankingApi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace InternetBankingRESTfulService.Api
+{
+    public class InternetBankingApi : IInternetBankingApi
+    {
+        public InternetBankingApi()
+        {
+
+        }
+
+        public string CalculateMD5(string value)
+        {
+            string replacedstar = Regex.Replace(value, "[0-9]", "X");
+
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] input = Encoding.ASCII.GetBytes(replacedstar);
+                byte[] hash = md5.ComputeHash(input);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public string GetApiVersion()
+        {
+            return DateTime.Now.ToUniversalTime().ToString("yyyy.MM.dd.1.0");
+        }
+
+        public bool IsPasswordStrong(string password)
+        {
+            if (password.Count() >= 12 &&
+                password.Any(Char.IsUpper) &&
+                password.Any(Char.IsLower) &&
+                password.Any(Char.IsDigit) &&
+               password.Any(c => c.Equals('*') &&
+                !password.Any(Char.IsWhiteSpace)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ InternetBankingRESTfulService.Web/Controllers/BankController.cs
@@ -0,0 +1,42 @@
+﻿using InternetBankingRESTfulService.Api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace InternetBankingRESTfulService.Web.Controllers
+{
+    [Route("bank")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+        IInternetBankingApi _internetBankingApi;
+
+        public BankController()
+        {
+            _internetBankingApi = new InternetBankingApi();
+        }
+
+        [HttpGet]
+        [Route("api/calc/MD5/{param}")]
+        [Route("api/calc/{param}/MD5")]
+        public IActionResult GetHash([FromRoute] string param)
+        {
+            return Ok(_internetBankingApi.CalculateMD5(param));
+        }
+
+        [HttpGet]
+        [Route("api/version")]
+        [Route("api-version")]
+        public IActionResult GetVersion()
+        {
+            return Ok(_internetBankingApi.GetApiVersion());
+        }
+
+        [HttpGet]
+        [Route("api/password/strong/{password}")]
+        [Route("api/is-password-strong/{password}")]
+        public IActionResult CheckPasswordStrong([FromRoute] string password)
+        {
+            return Ok(_internetBankingApi.IsPasswordStrong(password));
+        }
+    }
+}
